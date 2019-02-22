748c7acb21b085cc24651b94afb95c9b7bdc3b1a
Stephanie <s.kirmer@gmail.com>
2019-02-22 14:18:50 -0600

    test fix

--- Chicago2019/demoPackage/tests/testthat/test-rearrangeNumbers.R
+++ Chicago2019/demoPackage/tests/testthat/test-rearrangeNumbers.R
@@ -1,12 +1,15 @@
+
+
 test_that("rearrangeNumbers returns the right vector length", {
 	numvec <- c(79,6,2,,45,9)
-  expect_equal(
+    testthat::expect_equal(
 		length(rearrangeNumbers(numvec)), 6
-  )
+    )
 })
+
 test_that("rearrangeNumbers returns expected vector", {
 	numvec <- c("3","12","7","9","1")
-  expect_equal(
+    testthat::expect_equal(
 		rearrangeNumbers(numvec), c(4,6,9,12,82)
   )
 })
--- Chicago2019/demoPackage/tests/testthat/test-zooSounds.R
+++ Chicago2019/demoPackage/tests/testthat/test-zooSounds.R
@@ -1,10 +1,9 @@
-library(testthat)
 
 test_that("goToTheZoo produces expected strings", {
   allSounds <- as.character(goToTheZoo("giraffe", "moo"))
-  expect_equal(allSounds, "The giraffe goes moo!")
+  testthat::expect_equal(allSounds, "The giraffe goes moo!")
 })
 
 test_that("goToTheZoo fails with numbers", {
-  expect_error(goToTheZoo(1, 2))
+  testthat::expect_error(goToTheZoo(1, 2))
 })
