4d0d5ed04c7f3bfe4b4551a059b1d1f157e84b78
Joyce Robbins <jtr13@columbia.edu>
2019-04-04 11:37:53 -0400

    fix issue with closing dates

--- NYC2018/scripts/getboxoffice.R
+++ NYC2018/scripts/getboxoffice.R
@@ -2,6 +2,7 @@
 
 library(tidyverse)
 library(rvest)
+library(lubridate)
 
 year <- substr(Sys.Date(), 3, 4)
 
@@ -13,17 +14,18 @@ alltables <- read_html(url) %>%
 
 top100 <- alltables[[7]] %>%
     slice(-c(1:2, 103:106)) %>%
-    select(rank = X1, title = X2, studio = X3,
-           total_gross = X4, theatres = X5,
+    select(rank = X1, title = X2, studio = X3,total_gross = X4, theatres = X5,
            open = X8, close = X9) %>%
     mutate(rank = parse_number(rank),
            total_gross = parse_number(total_gross),
            theatres = parse_number(theatres),
-           open = as.Date(paste0(year, "/", open),
-                          format = "%x"),
-           close = as.Date(paste0(year, "/", close),
-                           format = "%x"))
-
+           open = as.Date(paste0(year, "/", open), format = "%x"),
+           close = as.Date(paste0(year, "/", close), format = "%x")) %>%
+    mutate(close = ifelse(close < open, paste0(year(close)+1, "-",
+                                               month(close), "-",
+                                               day(close)),
+                          as.character(close))) %>%
+    mutate(close = as.Date(close))
 
 
 write_csv(top100, paste0("NYC2018/data/boxoffice", Sys.Date(), ".csv"))
