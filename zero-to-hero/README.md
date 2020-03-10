57660c09de8f505a673b7c1f7514c47d7698f986
Heather Turner <ht@heatherturner.net>
2020-03-10 20:03:01 +0200

    add zero to hero workshop

---
+++ zero-to-hero/README.md
@@ -0,0 +1,6 @@
+# Introduction to R
+
+Materials for Introduction to R workshop
+
+http://www.igs2018.mu.edu.tr/en/introduction-to-r-3706
+
---
+++ zero-to-hero/slides/example_script.R
@@ -0,0 +1,17 @@
+# example commands
+View(iris)
+sum(2, 4, 7, 9)
+
+
+# summary -----------------------------------------------------------------
+
+summary(object = iris, maxsum = 2,
+        digits = 2)
+
+
+####################
+#**
+
+pbirthday(9, classes = 365, coincident = 3)
+
+
